eb487eb5e1902253f9ac497f161eadaebcd57bc0
Deazy06 <120008020+Deazy06@users.noreply.github.com>
2025-01-15 16:15:29 +0100

    Fullscreen Menu wowowowow

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -5,22 +5,53 @@ using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject menuCamera;
+    public GameObject targetObject;
+    public GameObject cameraObject;
+    private TheMove theMove;
+    private NewMove newMove;
+    private bool fadeOut;
+    public CanvasGroup canvasGroup;
+    public void Awake()
     {
+         theMove = targetObject.GetComponent<TheMove>();
+         newMove = cameraObject.GetComponent<NewMove>();
+
+         theMove.enabled = false;
+          newMove.enabled = false;
+    }
+
+    void FixedUpdate(){
+        if (fadeOut)
+        {
+            canvasGroup.alpha -= Time.deltaTime;
+        }
         
     }
+    public void StartGame(){
+        menuCamera.SetActive(false);
+        //this.gameObject.SetActive(false);
+        fadeOut = true;
+        StartCoroutine(delay(2f));
+    }
 
+    public void Settings(){
 
-    void StartGame(){
+    }
+
+    public void Quit(){
+
+    }
+
+    public void Fullscreen(){
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+    IEnumerator delay(float second){
+        yield return new WaitForSeconds(second);
         
-    }
-
-    void Settings(){
-
-    }
-
-    void Quit(){
-
+        theMove.enabled = true;
+        newMove.enabled = true;
+        this.gameObject.SetActive(false);
+        Debug.Log("lalalalaladelaydleay");
     }
 }
