c8bd210ab4f737e5d819446b8665bf389c5177f5
Deazy06 <120008020+Deazy06@users.noreply.github.com>
2025-01-15 13:44:02 +0100

    MainMenuWorkProgress

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void StartGame(){
+        
+    }
+
+    void Settings(){
+
+    }
+
+    void Quit(){
+
+    }
+}
