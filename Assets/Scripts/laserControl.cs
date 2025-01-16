8a44c3b13adafb769ceba74f85755f1c5b183e54
Deazy06 <120008020+Deazy06@users.noreply.github.com>
2025-01-16 16:15:49 +0100

    Revert "box"
    This reverts commit 70ac9ca34834dddee63b9537e5fbf554989607b2.

--- Assets/Scripts/laserControl.cs
+++ Assets/Scripts/laserControl.cs
@@ -1,4 +1,5 @@
-using System.Media;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class laserControl : MonoBehaviour
@@ -10,28 +11,17 @@ public class laserControl : MonoBehaviour
     [SerializeField]
     private bool reflectionOnlyMirrorIsPossible;
 
+    // Specify the tag of the object you want to trigger an action on hit
     [SerializeField]
     private string targetObjectTag = "TargetObject"; // Change this to your target object's tag
-
-    // Audio variables
-    [SerializeField]
-    private AudioClip hitSound; // Assign your sound effect in the inspector
-    private AudioSource audioSource;
-
-   
-    private bool soundPlayed = false;
-
-
-
 
     void Start()
     {
         lr = GetComponent<LineRenderer>();
         lr.SetPosition(0, startPoint.position);
-        audioSource = GetComponent<AudioSource>();
-        
     }
 
+    // Update is called once per frame
     void Update()
     {
         CastLaser(transform.position, -transform.forward);
@@ -51,25 +41,22 @@ public class laserControl : MonoBehaviour
                 direction = Vector3.Reflect(direction, hit.normal);
                 lr.SetPosition(i + 1, hit.point);
 
+                // Check if the hit object has the specified tag
                 if (hit.transform.CompareTag(targetObjectTag))
                 {
+                    // Trigger your action here
                     TriggerAction(hit.transform);
-                    break;
-                }
-
-                if (hit.transform.CompareTag("ResetPitchObject")) // Example tag to reset pitch
-                {
-                    
-                    soundPlayed = false;
+                    break; // Exit the loop if the target object is hit
                 }
 
                 if (hit.transform.CompareTag("Mirror") || !reflectionOnlyMirrorIsPossible)
                 {
-                    
+                    // Continue reflecting if it's a mirror or reflection is allowed
                     continue;
                 }
                 else
                 {
+                    // If it's not a mirror and reflection is not allowed, stop the laser
                     for (int j = (i + 1); j < maxBounces; j++)
                     {
                         lr.SetPosition(j, hit.point);
@@ -79,6 +66,7 @@ public class laserControl : MonoBehaviour
             }
             else
             {
+                // If no hit, set the last position to the end of the ray
                 lr.SetPosition(i + 1, position + direction * 100);
                 break;
             }
@@ -87,21 +75,11 @@ public class laserControl : MonoBehaviour
 
     void TriggerAction(Transform hitObject)
     {
+
         print("hitobject");
-        // Play the sound effect when the target object is hit
-        PlayHitSound();
+        // Implement the action you want to trigger when the laser hits the target object
+        //Debug.Log("Laser hit the target object: " + hitObject.name);
+        // You can start a class, call a method, or perform any action here
     }
 
-    void PlayHitSound()
-    {
-        if (!soundPlayed) // Check if the sound has already been played
-        {
-            print(audioSource);
-            
-            audioSource.PlayOneShot(hitSound);
-            
-            soundPlayed = true; // Set the flag to true to prevent further playback
-        }
-
-    }
 }
