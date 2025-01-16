1c8b9e3989e3554cbc632b943770eb3d356b380c
Deazy06 <120008020+Deazy06@users.noreply.github.com>
2025-01-16 15:50:34 +0100

    audioaudioaudioaudio

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -5,6 +5,12 @@ using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    #region variabels
+
+    public GameObject general_menu;
+    public GameObject graphics_menu;
+    public GameObject audio_menu;
+    //
     public GameObject coreMenu;
     public GameObject settingMenu;
     public GameObject menuCamera;
@@ -14,6 +20,7 @@ public class MenuManager : MonoBehaviour
     private NewMove newMove;
     private bool fadeOut;
     public CanvasGroup canvasGroup;
+    #endregion
     public void Awake()
     {
          theMove = targetObject.GetComponent<TheMove>();
@@ -30,6 +37,7 @@ public class MenuManager : MonoBehaviour
         }
         
     }
+    #region MainMenuButtons
     public void StartGame(){
         menuCamera.SetActive(false);
         //this.gameObject.SetActive(false);
@@ -47,9 +55,30 @@ public class MenuManager : MonoBehaviour
     }
 
     public void Quit(){
-
+        Application.Quit();
+    }
+    #endregion
+    
+    public void General(){
+        closeAllSettings();
+        general_menu.SetActive(true);
     }
 
+    public void Graphics(){
+        closeAllSettings();
+        graphics_menu.SetActive(true);
+    }
+
+    public void Audio(){
+        closeAllSettings();
+        audio_menu.SetActive(true);
+    }
+
+    void closeAllSettings(){
+        general_menu.SetActive(false);
+        graphics_menu.SetActive(false);
+        audio_menu.SetActive(false);
+    }
     public void Fullscreen(){
         Screen.fullScreen = !Screen.fullScreen;
     }
